be5c17724612e59def88f98736ce7b936540d615
lundjens <jenslund@037096029093.mbb.telenor.dk>
2013-09-21 17:00:20 +0200

    new

--- www/js/index.js
+++ www/js/index.js
@@ -45,5 +45,15 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+    },
+    
+    onDeviceReady: function() {
+        var element = document.getElementById('deviceProperties');
+        element.innerHTML = 'Device Name: '     + device.name     + '<br />' +
+                            'Device Cordova: '  + device.cordova  + '<br />' +
+                            'Device Platform: ' + device.platform + '<br />' +
+                            'Device UUID: '     + device.uuid     + '<br />' +
+                            'Device Version: '  + device.version  + '<br />';
     }
+
 };
